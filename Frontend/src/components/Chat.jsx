164a7ff3f30081a9e1f736d2fa6faa4f31f8a9b9
NHGamer <aniket94sharma56@gmail.com>
2026-05-24 18:28:08 +0530

    Added Header

--- Frontend/src/components/Chat.jsx
+++ Frontend/src/components/Chat.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Chat = () => {
   return (
-    <div>
-      
+    <div className='w-full h-screen bg-[#0a0b14]'>
+      <Header/>
     </div>
   )
 }
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import whiterobo from "../assets/white-robo.svg";
+
+const Header = () => {
+  return (
+    <div className="w-full border border-[#4c4c4c] flex  items-center justify-between">
+      <div className="flex items-center">
+        <div className="p-3 pl-7">
+          <img
+            className="p-2 bg-[#7c3aed] size-10 rounded-2xl"
+            src={whiterobo}
+            alt="image"
+          />
+        </div>
+        <div className="ml-2">
+          <h1 className="text-white pt-1">New writing session</h1>
+          <p className="text-[#757474] text-xs font-semibold">
+            Ai writing Assistant : Always improving.
+          </p>
+        </div>
+      </div>
+      <div className="btn-con ">
+        <button className=" text-white mr-10 px-4 rounded-2xl py-2 bg-[#7c3aed]">New Session</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
