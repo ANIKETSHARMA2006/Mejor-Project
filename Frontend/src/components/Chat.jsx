76dd1653257c6f73f5958cfd77a9c6fdc2b4b108
NHGamer <aniket94sharma56@gmail.com>
2026-05-26 14:51:37 +0530

    Added Effects

--- Frontend/src/components/Chat.jsx
+++ Frontend/src/components/Chat.jsx
@@ -5,7 +5,7 @@ import StarsBackground from './Stars'
 const Chat = () => {
   return (
     <div className='w-full h-screen '>
-      <div className='absolute w-170 h-170 pointer-events-none bg-purple-700/30  rounded-full blur-[140px] top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 animate-glow"'></div>
+      <div className='absolute w-175 h-145 pointer-events-none bg-purple-700/30  rounded-full blur-[140px] top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 animate-glow'></div>
       <StarsBackground/>
       <Header/>
     </div>
