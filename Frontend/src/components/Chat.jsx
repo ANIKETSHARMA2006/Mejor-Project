e4307e9fcf969c463abe3d98077cda9981a1b397
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-06-06 02:31:32 +0530

    correct css

--- Frontend/src/components/Chat.jsx
+++ Frontend/src/components/Chat.jsx
@@ -38,8 +38,8 @@ const Chat = () => {
 }
 
   return (
-    <div className='w-full h-screen flex justify-center items-center flex-col'>
-      <div className='absolute w-175 h-145 pointer-events-none bg-purple-700/30 rounded-full blur-[140px] top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 animate-glow'></div>
+    <div className='relative z-0 w-full h-screen flex justify-center items-center flex-col'>
+      <div className='absolute z-[-1] w-175 h-145 pointer-events-none bg-purple-700/30 rounded-full blur-[140px] top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 animate-glow'></div>
       <StarsBackground />
       <Header />
       {messages.length>0?<Chatsection messages={messages} />:<Open inputRef={inputRef} />}
