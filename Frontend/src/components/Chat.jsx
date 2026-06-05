b5ea6dadf84f4e868e39f43460da10b3e9bdfc42
NHGamer <aniket94sharma56@gmail.com>
2026-06-05 16:51:49 +0530

    latest Commit

--- Frontend/src/components/Chat.jsx
+++ Frontend/src/components/Chat.jsx
@@ -6,7 +6,7 @@ import Input from './Input'
 
 const Chat = () => {
   return (
-    <div className='w-full h-screen '>
+    <div className='w-full h-screen flex justify-center items-center flex-col'>
       <div className='absolute w-175 h-145 pointer-events-none bg-purple-700/30  rounded-full blur-[140px] top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 animate-glow'></div>
       <StarsBackground/>
       <Header/>
